81fbf85a7fa556e4df92415840ec415c1134b04e
jbaldacim <jbaldacim@gmail.com>
2026-03-11 11:43:24 -0300

    feat: complete Typography component system

--- src/components/Typography/Text.jsx
+++ src/components/Typography/Text.jsx
@@ -1,10 +1,61 @@
 import styled from "styled-components";
 
+export const Display = styled.h1`
+  font-family: ${({ theme }) => theme.fonts.sans};
+  font-size: ${({ theme }) => theme.fontSizes.xxl};
+  font-weight: ${({ theme }) => theme.fontWeights.light};
+  line-height: ${({ theme }) => theme.lineHeight.tight};
+  letter-spacing: ${({ theme }) => theme.letterSpacing.tight};
+  color: ${({ theme }) => theme.colors.text};
+`;
+
 export const Heading = styled.h2`
   font-family: ${({ theme }) => theme.fonts.sans};
   font-size: ${({ theme }) => theme.fontSizes.xl};
   font-weight: 300;
   line-height: ${({ theme }) => theme.lineHeight.tight};
   letter-spacing: ${({ theme }) => theme.letterSpacing.tight};
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+export const Lead = styled.p`
+  font-family: ${({ theme }) => theme.fonts.sans};
+  font-size: ${({ theme }) => theme.fontSizes.lg};
+  font-weight: ${({ theme }) => theme.fontWeights.light};
+  line-height: ${({ theme }) => theme.lineHeight.loose};
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+export const Body = styled.p`
+  font-family: ${({ theme }) => theme.fonts.sans};
+  font-size: ${({ theme }) => theme.fontSizes.md};
+  font-weight: ${({ theme }) => theme.fontWeights.regular};
+  line-height: ${({ theme }) => theme.lineHeight.normal};
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+export const Label = styled.span`
+  font-family: ${({ theme }) => theme.fonts.sans};
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+  font-weight: ${({ theme }) => theme.fontWeights.medium};
+  line-height: ${({ theme }) => theme.lineHeight.normal};
+  letter-spacing: ${({ theme }) => theme.letterSpacing.wider};
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.colors.muted};
+`;
+
+export const Caption = styled.span`
+  font-family: ${({ theme }) => theme.fonts.sans};
+  font-size: ${({ theme }) => theme.fontSizes.xs};
+  font-weight: ${({ theme }) => theme.fontWeights.regular};
+  line-height: ${({ theme }) => theme.lineHeight.normal};
+  color: ${({ theme }) => theme.colors.muted};
+`;
+
+export const InputText = styled.span`
+  font-family: ${({ theme }) => theme.fonts.mono};
+  font-size: ${({ theme }) => theme.fontSizes.md};
+  font-weight: ${({ theme }) => theme.fontWeights.regular};
+  line-height: ${({ theme }) => theme.lineHeight.loose};
   color: ${({ theme }) => theme.colors.text};
 `;
