620223765efca1d896b73cd1dd4f2734c8345e5d
jbaldacim <jbaldacim@gmail.com>
2026-03-23 17:57:28 -0300

    feat: add NewEntry view with form sections and fields

--- src/views/NewEntry.jsx
+++ src/views/NewEntry.jsx
@@ -1,5 +1,86 @@
+// TODO - Refactor layout
+import Container from "../components/Layout/Container";
+import Grid, { Col } from "../components/Layout/Grid";
+import Stack from "../components/Layout/Stack";
+import SectionHeader from "../components/SectionHeader/SectionHeader";
+import FloatingLabelField from "../components/Form/FloatingLabelField";
+import FloatingLabelCombobox from "../components/Form/FloatingLabelCombobox";
+import FloatingLabelTextArea from "../components/Form/FloatingLabelTextArea";
+import { Heading } from "../components/Typography/Text";
+
+const categories = ["Self-worth", "Family", "Relationship", "Work"];
+
 function NewEntry() {
-  return <div>NewEntry</div>;
+  return (
+    <Container>
+      <Stack>
+        <SectionHeader label="New Entry" heading="Record a new thought" />
+        <Grid>
+          <Col>
+            <Heading>The Assumption</Heading>
+          </Col>
+          <Col $span={8}>
+            <FloatingLabelField label="Tested assumption" id="assumption" />
+          </Col>
+          <Col $span={4}>
+            <FloatingLabelCombobox
+              label="Categories"
+              id="categories"
+              categories={categories}
+            />
+          </Col>
+          <Col>
+            <Heading>The Experiment</Heading>
+          </Col>
+          <Col>
+            <FloatingLabelTextArea label="Experiment" id="experiment" />
+          </Col>
+          <Col>
+            <FloatingLabelTextArea label="Predictions" id="predictions" />
+          </Col>
+          <Col>
+            <FloatingLabelTextArea
+              label="Possible problems"
+              id="possible-problems"
+            />
+          </Col>
+          <Col>
+            <FloatingLabelTextArea label="Strategies" id="strategies" />
+          </Col>
+          <Col>
+            <Heading>The Results</Heading>
+          </Col>
+          <Col>
+            <FloatingLabelTextArea label="What happened" id="what-happened" />
+          </Col>
+          <Col>
+            <FloatingLabelTextArea
+              label="Did results match predictions?"
+              id="results-vs-predictions"
+            />
+          </Col>
+          <Col>
+            <FloatingLabelTextArea
+              label="Anything unexpected?"
+              id="unexpected"
+            />
+          </Col>
+          <Col>
+            <FloatingLabelTextArea label="How you coped" id="how-coped" />
+          </Col>
+          <Col>
+            <Heading>The Conclusion</Heading>
+          </Col>
+          <Col>
+            <FloatingLabelTextArea
+              label="Alternative assumption"
+              id="alternative-assumption"
+            />
+          </Col>
+        </Grid>
+      </Stack>
+    </Container>
+  );
 }
 
 export default NewEntry;
